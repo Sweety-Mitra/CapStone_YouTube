31de75db9bfea9a543652b0d7728a569a141c59f
Sweety Mitra <118052909+Sweety-Mitra@users.noreply.github.com>
2026-01-13 23:45:37 +0530

    feat: render mock videos on home page

---
+++ src/data/mockVideos.js
@@ -0,0 +1,14 @@
+export const mockVideos = [
+  {
+    id: "1",
+    title: "Learn React in 30 Minutes",
+    channel: "Code with John",
+    views: "15K",
+  },
+  {
+    id: "2",
+    title: "JavaScript Basics",
+    channel: "JS Mastery",
+    views: "20K",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import MainLayout from "../layout/MainLayout";
+import VideoCard from "../components/video/VideoCard";
+import VideoGrid from "../components/video/VideoGrid";
+import { mockVideos } from "../data/mockVideos";
 
 const Home = () => {
   return (
     <MainLayout>
-      <h3>Home Page</h3>
+      <VideoGrid>
+        {mockVideos.map((video) => (
+          <VideoCard key={video.id} {...video} />
+        ))}
+      </VideoGrid>
     </MainLayout>
   );
 };

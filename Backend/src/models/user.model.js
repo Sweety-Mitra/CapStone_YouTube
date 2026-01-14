937da1edace68622207459849541df010798009e
Sweety Mitra <118052909+Sweety-Mitra@users.noreply.github.com>
2026-01-15 03:04:08 +0530

    feat: add user model for authentication

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+// This schema defines how user data is stored in MongoDB
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    avatar: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);

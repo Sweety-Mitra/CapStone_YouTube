1e6301e2b8ad4f09d84fe44790c70016e7ad2d39
Sweety Mitra <118052909+Sweety-Mitra@users.noreply.github.com>
2026-01-15 19:59:53 +0530

    feat: add channel model

---
+++ Backend/src/models/channel.model.js
@@ -0,0 +1,37 @@
+// Defines how channels are stored in MongoDB
+
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+  {
+    channelName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+
+    channelBanner: {
+      type: String,
+      default: "",
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Channel", channelSchema);
